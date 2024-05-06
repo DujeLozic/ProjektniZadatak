61a98b2a4f94774fc6d85fe928b26f47f98d90db
DujeLozic <dujelozic@gmail.com>
2024-05-06 13:35:07 +0200

    zaboravio sam dodat loader i stavio sam setTimeout 1s jer prebrzo load-a pa se ne primjeti :)

--- src/pages/Lecturers.tsx
+++ src/pages/Lecturers.tsx
@@ -2,19 +2,30 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { ILecturers } from "../interface";
 import LecturersFilters from "./components/LecturersComponents/LecturersFilters";
+import { ClipLoader } from "react-spinners";
 
 export const Lecturers = () => {
   const [lecturer, setLecturer] = useState<ILecturers[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get<ILecturers[]>("http://localhost:3001/lecturers")
-      .then((res) => setLecturer(res.data));
+    axios.get<ILecturers[]>("http://localhost:3001/lecturers").then((res) => {
+      setTimeout(() => {
+        setLecturer(res.data);
+        setLoading(false);
+      }, 1000);
+    });
   }, []);
 
   return (
     <div>
-      <LecturersFilters lecturer={lecturer} setLecturer={setLecturer} />
+      {loading === false ? (
+        <LecturersFilters lecturer={lecturer} setLecturer={setLecturer} />
+      ) : (
+        <div className="loaderContainer">
+          <ClipLoader />
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Workshops.tsx
+++ src/pages/Workshops.tsx
@@ -2,19 +2,32 @@ import { useEffect, useState } from "react";
 import { IWorkshops } from "../interface";
 import axios from "axios";
 import WorkshopFilters from "./components/workshopComponents/WorkshopFilters";
+import { ClipLoader } from "react-spinners";
+import "./Workshop.css";
 
 function Workshops() {
   const [workshop, setWorkshop] = useState<IWorkshops[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get<IWorkshops[]>("http://localhost:3001/workshops")
-      .then((res) => setWorkshop(res.data));
+    setLoading(true);
+    axios.get<IWorkshops[]>("http://localhost:3001/workshops").then((res) => {
+      setTimeout(() => {
+        setWorkshop(res.data);
+        setLoading(false);
+      }, 1000);
+    });
   }, []);
 
   return (
     <div>
-      <WorkshopFilters workshop={workshop} setWorkshop={setWorkshop} />
+      {loading === false ? (
+        <WorkshopFilters workshop={workshop} setWorkshop={setWorkshop} />
+      ) : (
+        <div className="loaderContainer">
+          <ClipLoader />
+        </div>
+      )}
     </div>
   );
 }
